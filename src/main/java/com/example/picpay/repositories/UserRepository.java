f83f1d9b1aab632513cb0409e4d914d40ef4986e
rafaeltavaares <rafaeltavsilva@gmail.com>
2024-06-03 09:40:32 -0300

    feat: create user repository

---
+++ src/main/java/com/example/picpay/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package com.example.picpay.repositories;
+
+import com.example.picpay.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findUserByDocument(String document);
+
+    Optional<User> findUserById(Long id);
+
+
+}

c6dd345e34910bfdb1ff68d8f7b165c14ac94d6d
rafaeltavaares <rafaeltavsilva@gmail.com>
2024-06-03 11:49:19 -0300

    feat: create transaction endpoint

--- src/main/java/com/example/picpay/DTOs/TransactionDTO.java
+++ src/main/java/com/example/picpay/DTOs/TransactionDTO.java
@@ -2,7 +2,7 @@ package com.example.picpay.DTOs;
 
 import java.math.BigDecimal;
 
-public record TransactionDTO(BigDecimal amount, Long sender_id, Long receiver_id) {
+public record TransactionDTO(Long sender_id, Long receiver_id, BigDecimal amount) {
 
 
 }
---
+++ src/main/java/com/example/picpay/controllers/TransactionController.java
@@ -0,0 +1,27 @@
+package com.example.picpay.controllers;
+
+import com.example.picpay.DTOs.TransactionDTO;
+import com.example.picpay.domain.transaction.Transaction;
+import com.example.picpay.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO data) throws Exception {
+        Transaction transaction = this.transactionService.createTransaction(data);
+        return new ResponseEntity<>(transaction, HttpStatus.OK);
+    }
+}

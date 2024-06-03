8e317fc79bfe0a02af5c24dc43f1f5651bc7d31f
rafaeltavaares <rafaeltavsilva@gmail.com>
2024-06-03 11:01:57 -0300

    feat: create User constructor to receive DTOs parameters

--- src/main/java/com/example/picpay/domain/user/User.java
+++ src/main/java/com/example/picpay/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.example.picpay.domain.user;
 
+import com.example.picpay.DTOs.UserDTO;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -34,4 +35,14 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO data){
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.email = data.email();
+        this.password = data.password();
+        this.document = data.document();
+        this.userType = data.userType();
+    }
 }
